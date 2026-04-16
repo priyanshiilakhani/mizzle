691b3c54485325c30b024df7f32e44b585b06281
PriyanshiLakhani <priyanshi.lakhani.it@gmail.com>
2026-04-16 18:07:30 +0530

    Done : sign-in

--- src/app/views/auth/sign-in/sign-in.ts
+++ src/app/views/auth/sign-in/sign-in.ts
@@ -1,36 +1,71 @@
 import { AuthService } from '@/app/services/auth.service';
 import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { Router, RouterLink } from "@angular/router";
-import { LucideAngularModule } from "lucide-angular";
+import { Router, RouterLink } from '@angular/router';
+import { LucideAngularModule } from 'lucide-angular';
 import { register } from 'swiper/element/bundle';
-import { FormsModule } from '@angular/forms'
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 register();
 
+const auth1 = '/images/auth/01.jpg';
+const auth2 = '/images/auth/02.jpg';
+const auth3 = '/images/auth/03.jpg';
+
+type SlideType = {
+  image: string;
+  text: string;
+  name: string;
+  role: string;
+  rating: number;
+};
 @Component({
   selector: 'app-sign-in',
-  imports: [RouterLink, LucideAngularModule,FormsModule],
+  imports: [RouterLink, LucideAngularModule, FormsModule, CommonModule],
   templateUrl: './sign-in.html',
   styles: ``,
-  schemas:[CUSTOM_ELEMENTS_SCHEMA]
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class SignIn {
-  email = 'admin@example.com'
-  password = 'password'
-  error = ''
+  email = 'admin@example.com';
+  password = 'password';
+  error = '';
 
-  constructor(private auth: AuthService, private router: Router) {}
+  constructor(
+    private auth: AuthService,
+    private router: Router,
+  ) {}
 
- onSubmit() {
-  const isLoggedIn = this.auth.login(
-    this.email,
-    this.password
-  );
+  onSubmit() {
+    const isLoggedIn = this.auth.login(this.email, this.password);
 
-  if (isLoggedIn) {
-    this.router.navigate(['']);
-  } else {
-    this.error = 'Invalid email or password';
+    if (isLoggedIn) {
+      this.router.navigate(['']);
+    } else {
+      this.error = 'Invalid email or password';
+    }
   }
-}
 
+  slideData: SlideType[] = [
+    {
+      image: auth1,
+      text: `"With unwavering determination, they navigate the complexities of the industry, forging strategic partnerships and driving transformative change."`,
+      name: 'Emma Watson',
+      role: 'Founder, catalog',
+      rating: 4.5,
+    },
+    {
+      image: auth2,
+      text: `"An exceptional agency CEO is a visionary, constantly pushing the boundaries of creativity and pushing their team to new heights. They inspire with their passion and cultivate a culture of trust and respect."`,
+      name: 'Carolyn Ortiz',
+      role: 'CEO, mizzle',
+      rating: 4.5,
+    },
+    {
+      image: auth3,
+      text: `"Through collaboration and strategic direction, they steer the agency towards its goals, navigating the ever-evolving landscape with agility and grace."`,
+      name: 'Dennis Barrett',
+      role: 'Founder, catalog',
+      rating: 4.5,
+    },
+  ];
 }
--- src/app/views/auth/sign-up/sign-up.ts
+++ src/app/views/auth/sign-up/sign-up.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
-
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { register } from 'swiper/element/bundle';
+register();
+// type SlideType = {
+//   image: string;
+//   text: string;
+//   name: string;
+//   role: string;
+//   rating: number;
+// };
 @Component({
   selector: 'app-sign-up',
   imports: [],
   templateUrl: './sign-up.html',
   styles: ``,
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class SignUp {
-
+  // slideData: SlideType[] = [
+  //   {
+  //     image: '/images/auth/01.jpg',
+  //     text: `"With unwavering determination, they navigate the complexities of the industry, forging strategic partnerships and driving transformative change."`,
+  //     name: 'Emma Watson',
+  //     role: 'Founder, catalog',
+  //   },
+  //   {
+  //     image: '/images/auth/02.jpg',
+  //     text: `"An exceptional agency CEO is a visionary, constantly pushing the boundaries of creativity and pushing their team to new heights. They inspire with their passion and cultivate a culture of trust and respect."`,
+  //     name: 'Carolyn Ortiz',
+  //     role: 'CEO, mizzle',
+  //   },
+  //   {
+  //     image: '/images/auth/03.jpg',
+  //     text: `"Through collaboration and strategic direction, they steer the agency towards its goals, navigating the ever-evolving landscape with agility and grace."`,
+  //     name: 'Dennis Barrett',
+  //     role: 'Founder, catalog',
+  //   },
+  // ];
 }
