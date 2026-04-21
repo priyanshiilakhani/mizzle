4c3ddfc18ea5e4830cee2c629e666cc70d8c96ef
PriyanshiLakhani <priyanshi.lakhani.it@gmail.com>
2026-04-21 10:41:46 +0530

    fix : coming-soon

--- src/app/views/pages/other-pages/coming-soon/coming-soon.ts
+++ src/app/views/pages/other-pages/coming-soon/coming-soon.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { LucideAngularModule } from "lucide-angular";
 import { RouterLink } from "@angular/router";
+import { Header } from "@/app/layout/components/header/header";
 
 @Component({
   selector: 'app-coming-soon',
-  imports: [LucideAngularModule, RouterLink],
+  imports: [LucideAngularModule, RouterLink, Header],
   templateUrl: './coming-soon.html',
   styles: ``,
 })
